8a18adbb64ec3f0db6d21efa2f12fe909504dd0e
chahatrajput5585 <106544104+chahatrajput5585@users.noreply.github.com>
2024-04-28 11:55:56 +0530

    Update applicationController.js

--- Backend_new/controllers/applicationController.js
+++ Backend_new/controllers/applicationController.js
@@ -8,42 +8,22 @@ export const postApplication = catchAsyncErrors(async (req, res, next) => {
   const { role } = req.user;
   if (role === "Employer") {
     return next(
-      new ErrorHandler("Employer not allowed to access this resource.", 400)
+      new ErrorHandler("Shopkeeper not allowed to access this resource.", 400)
     );
-  }
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return next(new ErrorHandler("Resume File Required!", 400));
   }
 
-  const { resume } = req.files;
-  const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
-  if (!allowedFormats.includes(resume.mimetype)) {
-    return next(
-      new ErrorHandler("Invalid file type. Please upload a PNG file.", 400)
-    );
-  }
-  const cloudinaryResponse = await cloudinary.uploader.upload(
-    resume.tempFilePath
-  );
 
-  if (!cloudinaryResponse || cloudinaryResponse.error) {
-    console.error(
-      "Cloudinary Error:",
-      cloudinaryResponse.error || "Unknown Cloudinary error"
-    );
-    return next(new ErrorHandler("Failed to upload Resume to Cloudinary", 500));
-  }
-  const { name, email, coverLetter, phone, address, jobId } = req.body;
+  const { name, email, cGSTLetter, phone, address, jobId } = req.body;
   const applicantID = {
     user: req.user._id,
     role: "Job Seeker",
   };
   if (!jobId) {
-    return next(new ErrorHandler("Job not found!", 404));
+    return next(new ErrorHandler("shop not found!", 404));
   }
   const jobDetails = await Job.findById(jobId);
   if (!jobDetails) {
-    return next(new ErrorHandler("Job not found!", 404));
+    return next(new ErrorHandler("shop not found!", 404));
   }
 
   const employerID = {
@@ -53,7 +33,7 @@ export const postApplication = catchAsyncErrors(async (req, res, next) => {
   if (
     !name ||
     !email ||
-    !coverLetter ||
+    !cGSTLetter ||
     !phone ||
     !address ||
     !applicantID ||
@@ -70,10 +50,7 @@ export const postApplication = catchAsyncErrors(async (req, res, next) => {
     address,
     applicantID,
     employerID,
-    resume: {
-      public_id: cloudinaryResponse.public_id,
-      url: cloudinaryResponse.secure_url,
-    },
+   
   });
   res.status(200).json({
     success: true,
@@ -87,7 +64,7 @@ export const employerGetAllApplications = catchAsyncErrors(
     const { role } = req.user;
     if (role === "Job Seeker") {
       return next(
-        new ErrorHandler("Job Seeker not allowed to access this resource.", 400)
+        new ErrorHandler("employer not allowed to access this resource.", 400)
       );
     }
     const { _id } = req.user;
